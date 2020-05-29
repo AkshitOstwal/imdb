c2152d432615acebfa973b186cf9f0935cd0a9c8
Akshit Ostwal <akshitostwal@gmail.com>
2020-05-29 18:39:00 +0530

    added rating from another api

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:imdb/Models/movie.dart';
 import 'package:imdb/providers/search_list_provider.dart';
 import 'package:provider/provider.dart';
@@ -10,79 +11,116 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final TextEditingController _searchData = TextEditingController();
-
+  final _formKey = GlobalKey<FormState>();
   Widget _buildMovieList(List<Movie> list, String error) {
     if (list.length == 0) {
-      return Center(
-          child: error != '' ? Text(error) : Text('Go do a search!!!!!'));
+      return Expanded(
+        child: Center(
+            child: error != '' ? Text(error) : Text('Go do a search!!!!!')),
+      );
     }
-    return ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: list.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            elevation: 10,
-            margin: EdgeInsets.symmetric(vertical: 20),
-            child: Container(
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(left: 30, bottom: 20),
-                    child: Container(
-                      height: 140,
-                      width: 100,
-                      child: OverflowBox(
-                        alignment: Alignment.bottomCenter,
-                        child: Image.network(
-                          list[index].posterURL,
+    return Expanded(
+      child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: list.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              elevation: 10,
+              margin: EdgeInsets.symmetric(vertical: 20),
+              child: Container(
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30, bottom: 20),
+                      child: Container(
+                        height: 140,
+                        width: 100,
+                        child: OverflowBox(
+                          alignment: Alignment.bottomCenter,
+                          child: Image.network(
+                            list[index].posterURL,
+                          ),
+                          maxWidth: 120,
+                          minHeight: 0,
+                          minWidth: 0,
+                          maxHeight: 160,
                         ),
-                        maxWidth: 120,
-                        minHeight: 0,
-                        minWidth: 0,
-                        maxHeight: 160,
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 40,
-                  ),
-                  Expanded(
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            list[index].title,
-                            style: TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.bold),
-                            overflow: TextOverflow.ellipsis,
-                            textAlign: TextAlign.start,
-                            maxLines: 2,
-                            softWrap: true,
-                          ),
-                          Text(
-                            list[index].year,
-                            style: TextStyle(fontSize: 12, color: Colors.grey),
-                            overflow: TextOverflow.ellipsis,
-                            textAlign: TextAlign.start,
-                            softWrap: true,
-                          ),
-                          Text(
-                            list[index].type,
-                            style: TextStyle(fontSize: 12, color: Colors.grey),
-                            overflow: TextOverflow.ellipsis,
-                            textAlign: TextAlign.start,
-                            softWrap: true,
-                          ),
-                        ],
-                      ),
+                    SizedBox(
+                      width: 40,
                     ),
-                  )
-                ],
+                    Expanded(
+                      child: Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              list[index].title,
+                              style: TextStyle(
+                                  fontSize: 22, fontWeight: FontWeight.bold),
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.start,
+                              maxLines: 2,
+                              softWrap: true,
+                            ),
+                            SizedBox(
+                              height: 12,
+                            ),
+                            Text(
+                              "Genre : ${list[index].genre}",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.grey),
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.start,
+                              softWrap: true,
+                            ),
+                            SizedBox(
+                              height: 12,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Text(
+                                  list[index].imdbRatings.toString(),
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.deepPurple,
+                                      fontWeight: FontWeight.bold),
+                                  overflow: TextOverflow.ellipsis,
+                                  textAlign: TextAlign.start,
+                                  softWrap: true,
+                                ),
+                                RatingBar(
+                                  initialRating: list[index].imdbRatings / 2,
+                                  direction: Axis.horizontal,
+                                  itemSize: 25,
+                                  allowHalfRating: true,
+                                  itemCount: 5,
+                                  itemPadding:
+                                      EdgeInsets.symmetric(horizontal: 4.0),
+                                  itemBuilder: (context, _) => Icon(
+                                    Icons.star,
+                                    color: Colors.deepOrange,
+                                  ),
+                                  glow: true,
+                                  glowColor: Colors.orange,
+                                  ignoreGestures: true,
+                                  tapOnlyMode: false,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          );
-        });
+            );
+          }),
+    );
   }
 
   @override
@@ -99,40 +137,60 @@ class _HomePageState extends State<HomePage> {
       },
       child: SafeArea(
         child: Scaffold(
-          appBar: AppBar(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-              side: BorderSide(
-                width: 2,
-              ),
-            ),
-            centerTitle: true,
-            backgroundColor: Colors.blue.shade50,
-            elevation: 0,
-            title: TextFormField(
-              decoration: InputDecoration(
-                hintText: "Search here",
-              ),
-              controller: _searchData,
-            ),
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(
-                  Icons.search,
-                  color: Colors.black,
-                ),
-                color: Colors.black,
-                onPressed: () => provider.getSearchList(_searchData.text),
-              )
-            ],
-          ),
           body: Container(
-            height: height,
-            width: width,
-            child: provider.loading
-                ? Center(child: Text('Loading....'))
-                : _buildMovieList(provider.list, provider.error),
-          ),
+              height: height,
+              width: width,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: AppBar(
+                      primary: true,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                        side: BorderSide(
+                          width: 1,
+                        ),
+                      ),
+                      centerTitle: true,
+                      backgroundColor: Colors.blue.shade50,
+                      elevation: 0,
+                      title: Form(
+                        key: _formKey,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "e.g Movie Name",
+                            helperText: "Movie Name",
+                            labelText: "Search",
+                          ),
+                          controller: _searchData,
+                          validator: (value) {
+                            if (value.isEmpty || value == null) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      actions: <Widget>[
+                        IconButton(
+                            icon: Icon(
+                              Icons.search,
+                              color: Colors.black,
+                            ),
+                            color: Colors.black,
+                            onPressed: () {
+                              if (_formKey.currentState.validate())
+                                provider.getSearchList(_searchData.text);
+                            })
+                      ],
+                    ),
+                  ),
+                  provider.loading
+                      ? Expanded(child: Center(child: Text('Loading....')))
+                      : _buildMovieList(provider.list, provider.error),
+                ],
+              )),
         ),
       ),
     );
--- lib/providers/search_list_provider.dart
+++ lib/providers/search_list_provider.dart
@@ -12,11 +12,11 @@ class SearchResultProvider with ChangeNotifier {
     return _loading;
   }
 
-  String get error{
+  String get error {
     return _error;
   }
 
-  List<Movie> get list{
+  List<Movie> get list {
     return List.castFrom(_list);
   }
 
@@ -29,8 +29,6 @@ class SearchResultProvider with ChangeNotifier {
     var result =
         await http.get('http://www.omdbapi.com/?apikey=$apikey&s=$data');
     var response = json.decode(result.body);
-    _loading = false;
-    notifyListeners();
     print("\n");
     if (response['Response'] == "False") {
       print(response['Error']);
@@ -39,19 +37,28 @@ class SearchResultProvider with ChangeNotifier {
       return false;
     } else {
       List<dynamic> _demoList = response['Search'];
-      _demoList.forEach((element) {
+      _demoList.forEach((element) async {
+        var result = await http.get(
+            'http://www.omdbapi.com/?apikey=$apikey&i=${element['imdbID']}');
+        var response = json.decode(result.body);
+
         Movie _demo = Movie();
         _demo.title = element['Title'];
         _demo.imdbID = element['imdbID'];
         _demo.year = element['Year'];
         _demo.type = element['Type'];
         _demo.posterURL = element['Poster'];
+        _demo.imdbRatings = double.parse(response['imdbRating']);
+        _demo.genre = response['Genre'];
         _list.add(_demo);
+        notifyListeners();
       });
+
+      _list.forEach((element) {
+        print(element.title);
+      });
+      _loading = false;
       notifyListeners();
-      // _list.forEach((element) {
-      //   print(element.title);
-      // });
       return true;
     }
   }
